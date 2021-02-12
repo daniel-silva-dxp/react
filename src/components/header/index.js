4f935f8ec74565e4fc105c07e6bd70a4098d90be
Daniel Silva <dani3l.dxp@gmail.com>
2021-02-12 18:28:09 -0300

    feat: criando componente Header

---
+++ src/components/header/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import "./main.css";
+
+export class Header extends Component {
+  render() {
+    return (
+      <header className="box">
+        <div className="container">
+          <h1>Curso ReactJS - Origamid</h1>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;

f437edbc5048e8f0194174a3b6850a16abf6bc79
Daniel Silva <dani3l.dxp@gmail.com>
2021-02-15 12:14:47 -0300

    feat: refatorando código

--- src/components/arrays/index.js
+++ src/components/arrays/index.js
@@ -6,19 +6,21 @@ const Arr = () => {
   const livros = [
     { id: 1, nome: "A Game of Thrones", ano: 1996 },
     { id: 2, nome: "A Clash of Kings", ano: 1998 },
-    { id: 1, nome: "A Storm of Swords", ano: 200 },
+    { id: 3, nome: "A Storm of Swords", ano: 200 },
   ];
 
   return (
-    <ul>
-      {livros.map(({ id, nome, ano }) => {
-        return (
-          <li key={id}>
-            <strong>Livro:</strong> {nome} - <strong>Ano:</strong> {ano}
-          </li>
-        );
-      })}
-    </ul>
+    <React.Fragment>
+      <ul>
+        {livros.map(({ id, nome, ano }) => {
+          return (
+            <li key={id}>
+              <strong>Livro:</strong> {nome} - <strong>Ano:</strong> {ano}
+            </li>
+          );
+        })}
+      </ul>
+    </React.Fragment>
   );
 };
 

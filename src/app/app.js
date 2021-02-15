8ce52d998aadd984c0005fff3e1f10ffc038de30
Daniel Silva <dani3l.dxp@gmail.com>
2021-02-15 16:15:16 -0300

    feat: testando o firebase

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
 import Home from "../pages";
+import firebase from "firebase";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      token: "Carregando",
+    };
+    var firebaseConfig = {};
+    // Initialize Firebase
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    } else {
+      firebase.app(); // if already initialized, use that one
+    }
+
+    firebase
+      .database()
+      .ref("token")
+      .on("value", (snapshot) => {
+        this.setState({
+          token: snapshot.val(),
+        });
+      });
+  }
   render() {
     return (
       <div>
+        <h1>{this.state.token}</h1>
         <Home />
       </div>
     );

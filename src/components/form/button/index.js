0f20bc1536dd7a194429911e694093d774180b5a
Daniel Silva <dani3l.dxp@gmail.com>
2021-02-17 11:52:52 -0300

    feat: estruturando e estilizando o componente button

--- src/components/form/button/index.js
+++ src/components/form/button/index.js
@@ -2,8 +2,12 @@ import React from "react";
 
 import "./main.css";
 
-const Button = ({ children }) => {
-  return <button className="btn btn-medium btn-dark">{children}</button>;
+const Button = ({ children, ...props }) => {
+  return (
+    <button {...props} className="btn btn-medium btn-purple">
+      {children}
+    </button>
+  );
 };
 
 export default Button;

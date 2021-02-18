9a20c31cd360d83c0d411b34e3690af918bcf643
Daniel Silva <dani3l.dxp@gmail.com>
2021-02-18 10:48:53 -0300

    feat: deletando arquivos

--- src/components/form/input/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./main.css";
-
-const Input = ({ handleChange, ...props }) => {
-  return <input onChange={handleChange} {...props} />;
-};
-
-export default Input;

36bab93a9547f242b769f0aba978d3b8bf8e1718
Daniel Silva <dani3l.dxp@gmail.com>
2021-02-23 20:49:37 -0300

    feat: implementando o footer

---
+++ src/Components/footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      © 2021 - Todos os direitos reservados.{" "}
+      <a href="https://www.danielsilva-dev.com.br/" target="blank">
+        Daniel Silva
+      </a>
+      .
+    </footer>
+  );
+};
+
+export default Footer;

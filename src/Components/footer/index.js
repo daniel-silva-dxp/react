9b724f9db6dd72aed892b33ffe5ef042f72a3dbc
Daniel Silva <dani3l.dxp@gmail.com>
2021-02-23 21:03:31 -0300

    feat: estilizando component footer

--- src/Components/footer/index.js
+++ src/Components/footer/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 
+import { Copyrights } from "./style";
+
 const Footer = () => {
   return (
-    <footer>
-      © 2021 - Todos os direitos reservados.{" "}
-      <a href="https://www.danielsilva-dev.com.br/" target="blank">
-        Daniel Silva
-      </a>
-      .
-    </footer>
+    <Copyrights>
+      <div className="container">
+        © 2021 - Todos os direitos reservados.{"  "}
+        <a href="https://www.danielsilva-dev.com.br/" target="blank">
+          Daniel Silva
+        </a>
+        .
+      </div>
+    </Copyrights>
   );
 };
 
---
+++ src/Components/footer/style.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Copyrights = styled.footer`
+  font-size: 14px;
+  width: 100%;
+  height: 60px;
+  background-color: #fff;
+  position: fixed;
+  bottom: 0;
+
+  border-top: 4px solid #75dab4;
+
+  .container {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    a {
+      color: #75dab4;
+      padding-left: 5px;
+    }
+  }
+`;

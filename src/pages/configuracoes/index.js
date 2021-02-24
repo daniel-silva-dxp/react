82c5ef49b10bb546104c143872ac58ce26184609
Daniel Silva <dani3l.dxp@gmail.com>
2021-02-24 12:46:48 -0300

    feat: configurando as páginas home e configurações

---
+++ src/pages/configuracoes/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import "./main.css";
+
+export default class Configuracoes extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Configuracoes</h1>
+      </div>
+    );
+  }
+}

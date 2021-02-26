a47f7d3a53065fd134cd94fd2fa7ab348bf8ff69
Daniel Silva <dani3l.dxp@gmail.com>
2021-02-26 16:02:02 -0300

    feat: iniciando a estrutura da page configuration

---
+++ src/pages/configuration/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import {} from "./styled";
+
+const Configuration = () => {
+  return (
+    <div className="container">
+      <h1>Configuration</h1>
+    </div>
+  );
+};
+
+export default Configuration;

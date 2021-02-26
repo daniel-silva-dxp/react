e4fec08157c22a6a8cb8fd90b59d987de751c850
Daniel Silva <dani3l.dxp@gmail.com>
2021-02-26 16:01:26 -0300

    feat: finalizando component sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaDev, FaHome, FaCog, FaPowerOff } from "react-icons/fa";
 import ReactTooltip from "react-tooltip";
+import { Link } from "react-router-dom";
 
 import { Aside, Nav } from "./styled";
 
@@ -12,30 +13,34 @@ const Sidebar = () => {
           <FaDev />
         </div>
         <ul>
-          <li data-tip data-for="home">
-            <FaHome />
-            <ReactTooltip
-              id="home"
-              textColor="#fff"
-              backgroundColor="#5965e0"
-              place="right"
-              effect="solid"
-            >
-              <span>Home</span>
-            </ReactTooltip>
-          </li>
-          <li data-tip data-for="config">
-            <FaCog />
-            <ReactTooltip
-              id="config"
-              textColor="#fff"
-              backgroundColor="#5965e0"
-              place="right"
-              effect="solid"
-            >
-              <span>Configuration</span>
-            </ReactTooltip>
-          </li>
+          <Link to="/">
+            <li data-tip data-for="home">
+              <FaHome />
+              <ReactTooltip
+                id="home"
+                textColor="#fff"
+                backgroundColor="#5965e0"
+                place="right"
+                effect="solid"
+              >
+                <span>Home</span>
+              </ReactTooltip>
+            </li>
+          </Link>
+          <Link to="/config">
+            <li data-tip data-for="config">
+              <FaCog />
+              <ReactTooltip
+                id="config"
+                textColor="#fff"
+                backgroundColor="#5965e0"
+                place="right"
+                effect="solid"
+              >
+                <span>Configuration</span>
+              </ReactTooltip>
+            </li>
+          </Link>
           <li data-tip data-for="logout">
             <FaPowerOff />
             <ReactTooltip
